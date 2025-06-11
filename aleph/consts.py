ed46d0c0a075cd183fe2a0022eec038666537637
sagnikc395 <sagnikchatterjee395@gmail.com>
2025-06-11 23:20:08 +0530

    change to uv

--- aleph/consts.py
+++
@@ -1,8 +0,0 @@
-# ------------------------------------------------------------------------------
-# Instantiate Protocol Objects from the patterns Directory
-# ------------------------------------------------------------------------------
-from pathlib import Path
-
-
-PATTERNS_DIR = Path("./Patterns")
-RESERVOIR_DIR = Path("./Reservoir")
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from aleph!")
+
+
+if __name__ == "__main__":
+    main()
